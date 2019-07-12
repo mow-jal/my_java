bd91a1922fdb736673e97b828f8e418a1808b53b
WaRaShaYa <WaRaShaYa@DESKTOP-P8OUDU4.fios-router.home>
2019-07-12 14:02:11 -0400

    fixed jsonutils.java

--- src/test/java/test/JsonUtils.java
+++ src/test/java/test/JsonUtils.java
@@ -84,20 +84,20 @@ public class JsonUtils {
 	
 	
 	public void buildLotto(int lottoid, int winnerId) {
-		String response1 = 		   
-				"{" +
-				"	\"lotto\":{" +
-				"	 \"lottoId\":5"," +
-				"	 \"winning_numbers\":[2,45,34,23,7,5,3]," +
-				"	 \"winners\":[{" +
-				"	   \"winnerId\":23," +
-				"	   \"numbers\":[2,45,34,23,3,5]" +
-				"	 },{" +
-				"	   \"winnerId\":54," +
-				"	   \"numbers\":[52,3,12,11,18,22]" +
-				"	 }]" +
-				"	}" +
-				"}";
+//		String response1 = 		   
+//				"{" +
+//				"	\"lotto\":{" +
+//				"	 \"lottoId\":5"," +
+//				"	 \"winning_numbers\":[2,45,34,23,7,5,3]," +
+//				"	 \"winners\":[{" +
+//				"	   \"winnerId\":23," +
+//				"	   \"numbers\":[2,45,34,23,3,5]" +
+//				"	 },{" +
+//				"	   \"winnerId\":54," +
+//				"	   \"numbers\":[52,3,12,11,18,22]" +
+//				"	 }]" +
+//				"	}" +
+//				"}";
 		
 	}
 	
